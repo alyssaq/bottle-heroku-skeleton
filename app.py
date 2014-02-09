45841ddd025d9f2e93bd6910d76c39f57a252880
alyssaq <alyssaquek@gmail.com>
2014-02-09 20:17:36 +0800

    Added corpora for textblob

--- app.py
+++ app.py
@@ -11,16 +11,31 @@ from bottle import default_app, request, route, response, get
 from textblob import TextBlob
 import pandas as pd
 import numpy as np
-
+import sys
+from textblob.packages import nltk
 #import appconfig
 
 bottle.debug(True)
 
+MIN_CORPORA = [
+    'brown',  # Required for FastNPExtractor
+    'punkt',  # Required for WordTokenizer
+    'wordnet' # Required for lemmatization
+]
+
+def download_lite():
+  for each in MIN_CORPORA:
+    print('Downloading "{0}"'.format(each))
+    nltk.download(each)
+
+download_lite()
+       
 @get('/')
 def index():
   response.content_type = 'text/plain; charset=utf-8'
-  blob = TextBlob("Now is better than never.")
+  
   data = pd.DataFrame({'A': np.random.randn(3), 'B': np.random.randn(3)})
+  blob = TextBlob("Now is better than never.")
 
   ret =  'Hello world, I\'m {0}!\n\n'.format(os.getpid())
   for word_list in blob.ngrams(n=3):
